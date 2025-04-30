08253b5d2661b7c87872a6a3b4a29fef4df8dc0b
antazoey <antyzoa@gmail.com>
2025-04-30 12:13:38 -0500

    chore: setup project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# tplus-client
-A Python client for interacting with tplus.
+# TPlus Python Client Utilities
+
+Python clients for interacting with tplus.
+
+## Install
+
+To install, use either `pip` or `uv pip`:
+
+```shell
+uv pip install -e .
+```
---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+here = Path(__file__).parent.absolute()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="tpluspy",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description="""tpluspy: Client utilities for interacting with tplus""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="TPlus Labs",
+    author_email="admin@tlus.cx",
+    url="https://github.com/tpluslabs/tpluspy",
+    include_package_data=True,
+    install_requires=[
+        "eth-ape>=0.8.32,<0.9",
+    ],
+    python_requires=">=3.9,<4",
+    extras_require={
+        "test": [
+            "pytest>=6.0",
+            "pytest-timeout>=2.2.0,<3",
+            "pytest-mock",
+        ],
+        "lint": [
+            "mypy>=1.15.0,<2",
+            "ruff>=0.11.7",
+            "mypy>=1.15.0,<2",
+        ],
+        "release": [
+            "setuptools>=75.6.0",
+            "wheel",
+            "twine",
+        ],
+    },
+    py_modules=["tpluspy"],
+    license="Apache-2.0",
+    zip_safe=False,
+    keywords="ethereum",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={"tpluspy": ["py.typed"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+)
