bec8a6aca88a517e73d205a908ba03e3218829df
antazoey <antyzoa@gmail.com>
2025-05-02 08:36:40 -0500

    test: sign in a test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,6 @@
 import pytest
-from ape import convert
-
-from tplus.evm.utils import address_to_bytes32
-from tplus.evm.eip712 import Order
 
 
 @pytest.fixture
 def signer(accounts):
     return accounts[0]
-
-
-@pytest.fixture
-def order(signer):
-    return Order(
-        tokenOut="0x62622E77D1349Face943C6e7D5c01C61465FE1dc",
-        amountOut=convert("1 ether", int),
-        tokenIn="0x58372ab62269A52fA636aD7F200d93999595DCAF",
-        amountIn=convert("1 ether", int),
-        userId=address_to_bytes32(signer.address),
-        nonce=1,
-        validUntil=1000000000,
-    )
-
--- tests/test_evm.py
+++ tests/test_evm.py
@@ -1,3 +1,21 @@
-def test_settlement_order(signer, order):
+from ape import convert
+
+from tplus.evm.eip712 import Order
+from tplus.evm.utils import address_to_bytes32
+
+
+def test_settlement_order(signer):
+    """
+    Mostly for demo-ing how to sign an order.
+    """
+    order = Order(
+        tokenOut="0x62622E77D1349Face943C6e7D5c01C61465FE1dc",
+        amountOut=convert("1 ether", int),
+        tokenIn="0x58372ab62269A52fA636aD7F200d93999595DCAF",
+        amountIn=convert("1 ether", int),
+        userId=address_to_bytes32(signer.address),
+        nonce=1,
+        validUntil=1000000000,
+    )
     signature = signer.sign_message(order)
     assert signer.check_signature(order, signature)
