f6b997da84403da88ef96fc037c04d99c36070b2
antazoey <antyzoa@gmail.com>
2025-05-06 09:40:13 -0500

    fix: wrong name in msg

--- tplus/utils/limit_order.py
+++ tplus/utils/limit_order.py
@@ -17,7 +17,7 @@ def create_limit_order(
     fill_or_kill=False,
     asset_index=200,
     order_id: Optional[str] = None,
-    post_only: bool = False
+    post_only: bool = False,
 ):
     order_id = str(uuid.uuid4()) if order_id is None else order_id
     asset = IndexAsset(Index=asset_index)
