a60e143b43994bca6c8443b81490bec6747c5a13
antazoey <antyzoa@gmail.com>
2025-05-20 12:31:58 -0500

    fix: right pad bytes32 instead

--- tplus/evm/utils.py
+++ tplus/evm/utils.py
@@ -4,8 +4,9 @@ from ape.types import AddressType
 def address_to_bytes32(address: str | AddressType) -> bytes:
     """
     In t+, address keys are typically bytes32 to be consistent across chain.
-    Use this utility to convert EVM address types to bytes32 by left
-    padding with zeroes.
+    Use this utility to convert EVM address types to bytes32 by right
+    padding with zeroes. We use right-pad because that is how bytes32 values
+    are typically stored on chain, as they are not actually scalar types.
 
     Args:
         address (str | AddressType): The typical size-20 EVM address.
@@ -14,4 +15,4 @@ def address_to_bytes32(address: str | AddressType) -> bytes:
         bytes: Size 32.
     """
     addr_bytes = bytes.fromhex(address[2:])
-    return addr_bytes.rjust(32, b"\x00")
+    return addr_bytes.ljust(32, b"\x00")
