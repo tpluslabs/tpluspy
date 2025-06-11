afcfa824b5324380b72c578ceb2e61a11cc21ff7
antazoey <antyzoa@gmail.com>
2025-06-11 16:56:56 -0500

    feat: init settlement

--- tplus/client/clearingengine.py
+++ tplus/client/clearingengine.py
@@ -1,4 +1,5 @@
 from tplus.client.base import BaseClient
+from tplus.model.asset_identifier import AssetIdentifier
 
 
 class ClearingEngineClient(BaseClient):
@@ -18,3 +19,27 @@ class ClearingEngineClient(BaseClient):
 
     async def update_risk_parameters(self, registry_chain_id: int):
         return self._request("POST", "params/update", json={"registry_chain_id": registry_chain_id})
+
+    async def init_settlement(
+        self,
+        user: str,
+        calldata: str,
+        asset_in: AssetIdentifier,
+        amount_in: int,
+        asset_out: AssetIdentifier,
+        amount_out: int,
+        chain_id: int,
+        signature: str,
+    ):
+        inner = {
+            "tplus_user": user,
+            "calldata": calldata,
+            "asset_in": asset_in,
+            "amount_in": amount_in,
+            "asset_out": asset_out,
+            "amount_out": amount_out,
+            "chain_id": chain_id,
+        }
+        return await self._request(
+            "POST", "settlement/init", json_data={"inner": inner, "signature": signature}
+        )
