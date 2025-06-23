ca5ade3a6f1f573a8754438a1d6c969ca7929ae2
antazoey <antyzoa@gmail.com>
2025-06-23 08:21:19 -0500

    fix: accidentally required ape

--- tests/evm/test_eip712.py
+++ tests/evm/test_eip712.py
@@ -13,7 +13,7 @@ def test_settlement_order(signer):
         amountOut=convert("1 ether", int),
         tokenIn="0x58372ab62269A52fA636aD7F200d93999595DCAF",
         amountIn=convert("1 ether", int),
-        userId=to_bytes32(signer.address),
+        user=to_bytes32(signer.address),
         nonce=1,
         validUntil=1000000000,
     )
