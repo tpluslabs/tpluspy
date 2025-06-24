d54ba1eb271448e14cd6649132d2e4bb36efdbbe
antazoey <antyzoa@gmail.com>
2025-06-24 09:52:57 -0500

    fix: handle unpadded asset ids better

--- tests/model/test_asset_identifier.py
+++ tests/model/test_asset_identifier.py
@@ -2,7 +2,78 @@ from tplus.model.asset_identifier import AssetIdentifier
 
 
 class TestAssetIdentifier:
-    def test_str(self):
+    def test_index_str(self):
         asset_id = AssetIdentifier("0")
         actual = f"{asset_id}"
         assert actual == "0"
+
+    def test_address_str(self):
+        """
+        Shows it automatically pads and utilizes an asset address str.
+        """
+        raw_str = (
+            "62622e77d1349face943c6e7d5c01c61465fe1dc000000000000000000000000@aa36a70000000000"
+        )
+        asset_id = AssetIdentifier(raw_str)
+        actual = asset_id.model_dump()
+        assert actual == raw_str
+
+    def test_address_str_unpadded_address(self):
+        """
+        Shows it automatically pads and utilizes an asset address str.
+        """
+        raw_str = "62622E77D1349Face943C6e7D5c01C61465FE1dc@aa36a70000000000"
+        asset_id = AssetIdentifier(raw_str)
+        actual = asset_id.model_dump()
+        expected = (
+            "62622e77d1349face943c6e7d5c01c61465fe1dc000000000000000000000000@aa36a70000000000"
+        )
+        assert actual == expected
+
+    def test_address_str_unpadded_chain(self):
+        """
+        Shows it automatically pads and utilizes an asset address str.
+        """
+        raw_str = "62622e77d1349face943c6e7d5c01c61465fe1dc000000000000000000000000@aa36a7"
+        asset_id = AssetIdentifier(raw_str)
+        actual = asset_id.model_dump()
+        expected = (
+            "62622e77d1349face943c6e7d5c01c61465fe1dc000000000000000000000000@aa36a70000000000"
+        )
+        assert actual == expected
+
+    def test_address_decimal_chain(self):
+        """
+        Shows it automatically pads and utilizes an asset address str.
+        """
+        raw_str = "62622e77d1349face943c6e7d5c01c61465fe1dc000000000000000000000000@11155111"
+        asset_id = AssetIdentifier(raw_str)
+        actual = asset_id.model_dump()
+        expected = (
+            "62622e77d1349face943c6e7d5c01c61465fe1dc000000000000000000000000@aa36a70000000000"
+        )
+        assert actual == expected
+
+    def test_address_decimal_chain_and_unpadded_address(self):
+        """
+        Shows it automatically pads and utilizes an asset address str.
+        """
+        raw_str = "62622e77d1349face943c6e7d5c01c61465fe1dc@11155111"
+        asset_id = AssetIdentifier(raw_str)
+        actual = asset_id.model_dump()
+        expected = (
+            "62622e77d1349face943c6e7d5c01c61465fe1dc000000000000000000000000@aa36a70000000000"
+        )
+        assert actual == expected
+
+    def test_address_str_unpadded_both_sides(self):
+        """
+        Shows it automatically pads and utilizes an asset address str.
+        """
+        raw_str = "62622E77D1349Face943C6e7D5c01C61465FE1dc@aa36a7"
+        asset_id = AssetIdentifier(raw_str)
+        actual = asset_id.model_dump()
+        expected = (
+            "62622e77d1349face943c6e7d5c01c61465fe1dc000000000000000000000000@aa36a70000000000"
+        )
+        assert actual == expected
--- tests/model/test_withdrawal.py
+++ tests/model/test_withdrawal.py
@@ -14,6 +14,6 @@ class TestWithdrawal:
             chain_id=11155111,
         )
         withdrawal = WithdrawalRequest(inner=inner, signature=[])
-        actual = withdrawal.model_dump_json()
-        expected = '{"inner":{"tplus_user":[235,136,106,86,249,240,239,166,68,50,103,140,235,241,39,14,147,20,167,88,230,235,105,122,96,98,2,164,81,227,232,46],"asset":"62622E77D1349Face943C6e7D5c01C61465FE1dc@aa36a7","amount":100,"target":"0x62622e77d1349face943c6e7d5c01c61465fe1dc000000000000000000000000","chain_id":11155111},"signature":[]}'
+        actual = withdrawal.inner.model_dump_json()
+        expected = '{"tplus_user":[235,136,106,86,249,240,239,166,68,50,103,140,235,241,39,14,147,20,167,88,230,235,105,122,96,98,2,164,81,227,232,46],"asset":"62622e77d1349face943c6e7d5c01c61465fe1dc000000000000000000000000@aa36a700000000","amount":"0x64","target":"0x62622e77d1349face943c6e7d5c01c61465fe1dc000000000000000000000000","chain_id":[0,0,0,0,0,170,54,167]}'
         assert actual == expected
