bb8e4900aca27221a409dd8c594c3a7debeb7ecb
antazoey <antyzoa@gmail.com>
2025-06-24 08:47:54 -0500

    feat: wip

--- tplus/model/withdrawal.py
+++ tplus/model/withdrawal.py
@@ -1,10 +1,11 @@
 from typing import TYPE_CHECKING
 
-from pydantic import BaseModel
+from eth_pydantic_types.hex.str import HexStr32
+from pydantic import BaseModel, field_serializer
 
 from tplus.model.asset_identifier import AssetIdentifier
 from tplus.utils.bytes32 import to_bytes32
-from tplus.utils.hex import str_to_vec
+from tplus.utils.hex import str_to_vec, to_vec
 
 if TYPE_CHECKING:
     from tplus.utils.user import User
@@ -14,8 +15,16 @@ class InnerWithdrawalRequest(BaseModel):
     tplus_user: str
     asset: AssetIdentifier
     amount: int
-    target: str
+    target: HexStr32
     chain_id: int
+
+    @field_serializer("tplus_user", when_used="json")
+    def serialize_user(self, user):
+        return to_vec(user)
+
+    @field_serializer("tplus_user", when_used="json")
+    def serialize_user(self, user):
+        return to_vec(user)
 
 
 class WithdrawalRequest(BaseModel):
@@ -44,5 +53,8 @@ class WithdrawalRequest(BaseModel):
                 "signature": [],
             }
         )
-        model.signature = str_to_vec(signer.sign(model.inner.model_dump_json()).hex())
+        model.signature = str_to_vec(signer.sign(model.signing_payload()).hex())
         return model
+
+    def signing_payload(self) -> str:
+        return self.inner.model_dump_json(exclude_none=True).replace(" ", "").replace("\r", "").replace("\n", "")
