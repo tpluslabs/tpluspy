c0277be455a92c77a994164aca02f3b4fcba1b8b
antazoey <antyzoa@gmail.com>
2025-07-09 09:00:35 -0500

    fix: bundle settlement models

--- tests/model/test_settlement.py
+++ tests/model/test_settlement.py
@@ -7,12 +7,13 @@ ASSET_OUT = "0x58372ab62269A52fA636aD7F200d93999595DCAF"
 
 
 class TestTxSettlementRequest:
-    def test_model_dump_json(self):
+    def test_signing_payload(self):
         """
         Show we serialize to the JSON the clearing-engine expects.
         """
+        settler = "0xeb886a56f9f0efa64432678cebf1270e9314a758e6eb697a606202a451e3e82e"
         settlement = {
-            "tplus_user": "0xeb886a56f9f0efa64432678cebf1270e9314a758e6eb697a606202a451e3e82e",
+            "tplus_user": settler,
             "calldata": [],
             "asset_in": AssetIdentifier(f"{ASSET_IN}@{CHAIN_ID}"),
             "amount_in": 100,
@@ -21,6 +22,6 @@ class TestTxSettlementRequest:
             "chain_id": CHAIN_ID,
         }
         settlement = TxSettlementRequest(inner=settlement, signature=[])
-        actual = settlement.inner.model_dump_json()
+        actual = settlement.inner.signing_payload(settler)
         expected = '{"tplus_user":[235,136,106,86,249,240,239,166,68,50,103,140,235,241,39,14,147,20,167,88,230,235,105,122,96,98,2,164,81,227,232,46],"calldata":[],"asset_in":"62622e77d1349face943c6e7d5c01c61465fe1dc000000000000000000000000@0000000000aa36a7","amount_in":"0x64","asset_out":"58372ab62269a52fa636ad7f200d93999595dcaf000000000000000000000000@0000000000aa36a7","amount_out":"0x64","chain_id":[0,0,0,0,0,170,54,167]}'
         assert actual == expected
--- tplus/model/settlement.py
+++ tplus/model/settlement.py
@@ -1,19 +1,79 @@
+import json
+from typing import TYPE_CHECKING
+
 from eth_pydantic_types.hex.int import HexInt
 from pydantic import BaseModel
 
 from tplus.model.asset_identifier import AssetIdentifier
 from tplus.model.types import ChainID, UserPublicKey
+from tplus.utils.hex import str_to_vec
+
+if TYPE_CHECKING:
+    from tplus.utils.user import User
 
 
 class TxSettlementRequest(BaseModel):
+    """
+    Atomic settlement request.
+    """
+
     inner: "InnerSettlementRequest"
+    """
+    The inner part of the request (signature fields).
+    """
+
     signature: list[int]
+    """
+    The settler's signature from signing the necessary data (mostly from ``.inner``).
+    """
+
+    @classmethod
+    def create_signed(
+        cls, inner: "InnerSettlementRequest", signer: "User"
+    ) -> "TxSettlementRequest":
+        signature = str_to_vec(signer.sign(inner.signing_payload()).hex())
+        return cls(inner=inner, signature=signature)
+
+    def signing_payload(self) -> str:
+        return self.inner.signing_payload()
+
+
+class BundleSettlementRequest(BaseModel):
+    """
+    Bundle settlement request.
+    """
+
+    inner: list["InnerBundleSettlementRequest"]
+    """
+    The inner part of the request (signature fields).
+    Allows multiple settlements, unlike ``TxSettlementRequest``.
+    """
+
+    bundle: "SimBundleRequest"
+    """
+    The MEV EVM standard bundle that fulfills the settlement (requires verification
+    via mev_simBundle RPC).
+    """
+
+    signature: list[int]
+    """
+    The settler's signature from signing the necessary data (mostly from ``.inner``).
+    """
+
+    user: UserPublicKey
+    """
+    The settler.
+    """
+
+    chain_id: int
+    """
+    The chain ID of the deposit vault settling on.
+    """
 
 
 class BaseInnerSettlementRequest(BaseModel):
-    tplus_user: UserPublicKey
     """
-    The settler.
+    The shared fields for all inner settlement requests.
     """
 
     asset_in: AssetIdentifier
@@ -21,38 +81,47 @@ class BaseInnerSettlementRequest(BaseModel):
     asset_out: AssetIdentifier
     amount_out: HexInt
 
-    chain_id: ChainID
-    """
-    The chain ID of the settlement (can settle cross-chain).
-    """
 
-
-class InnerSettlementRequest(BaseModel):
+class InnerSettlementRequest(BaseInnerSettlementRequest):
     """
-    Inner settlement request part of atomic settlement.
+    Atomic settlement inner request. Additionally, contains calldata for an on-chain
+    settlement callback.
     """
 
     calldata: list[HexInt]
+    chain_id: ChainID
+
+    def signing_payload(self, settler: UserPublicKey) -> str:
+        base_data = self.model_dump(mode="json", exclude_none=True)
+        calldata = base_data.pop("calldata", [])
+
+        # NOTE: The order here matters!
+        payload = {
+            "tplus_user": str_to_vec(settler),
+            "calldata": calldata,
+            **base_data,
+        }
+
+        return json.dumps(payload).replace(" ", "").replace("\r", "").replace("\n", "")
 
 
 class InnerBundleSettlementRequest(BaseInnerSettlementRequest):
     """
-    Inner settlement request part of bundle settlement.
-    """
-
-
-class BundleSettlementRequest(BaseModel):
-    inner: list["InnerBundleSettlementRequest"]
-    bundle: "SimBundleRequest"
-    signature: list[int]
-
-    chain_id: ChainID
-    """
-    The chain ID of the deposit vault settling on.
+    Bundle settlement inner request. Does not contain any additional fields.
     """
 
 
 class SimBundleRequest(BaseModel):
+    """
+    Data for the light-client to validate the transaction bundle.
+    """
+
     bundle: dict  # NOTE: Not using the model here so Ape isn't required.
-    user: UserPublicKey
+    """
+    Bundle data. Should match the model expected in RPC ``mev_SimBundle``.
+    """
+
     bundle_id: int
+    """
+    A bundle identifier, should be unique for all settlement.
+    """
