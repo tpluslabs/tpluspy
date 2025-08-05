e0f6900a7bbc59ece2ddf6cddfa8096a3dc6d7b6
antazoey <admin@antazoey.me>
2025-08-05 13:53:28 -0500

    feat: `TxSettlementRequest.create_signed()` accept raw dictionary (#46)

--- tests/model/test_settlement.py
+++ tests/model/test_settlement.py
@@ -1,50 +1,61 @@
+import pytest
+
 from tplus.model.asset_identifier import AssetIdentifier
 from tplus.model.settlement import (
     BundleSettlementRequest,
     TxSettlementRequest,
 )
+from tplus.utils.hex import to_vec
 
 CHAIN_ID = 11155111
 ASSET_IN = "0x62622E77D1349Face943C6e7D5c01C61465FE1dc"
 ASSET_OUT = "0x58372ab62269A52fA636aD7F200d93999595DCAF"
-SETTLER = "0xeb886a56f9f0efa64432678cebf1270e9314a758e6eb697a606202a451e3e82e"
 
 
 class TestTxSettlementRequest:
-    def test_signing_payload(self):
-        """
-        Show we serialize to the JSON the clearing-engine expects.
-        """
-        settlement = {
-            "tplus_user": SETTLER,
+    @pytest.fixture(scope="class")
+    def settlement(self, user):
+        return {
+            "tplus_user": user.public_key,
             "calldata": [],
             **get_base_settlement_data(),
             "chain_id": CHAIN_ID,
         }
+
+    def test_signing_payload(self, settlement, user):
+        """
+        Show we serialize to the JSON the clearing-engine expects.
+        """
         settlement = TxSettlementRequest(inner=settlement, signature=[])
         actual = settlement.signing_payload()
-        expected = '{"tplus_user":[235,136,106,86,249,240,239,166,68,50,103,140,235,241,39,14,147,20,167,88,230,235,105,122,96,98,2,164,81,227,232,46],"calldata":[],"asset_in":"62622e77d1349face943c6e7d5c01c61465fe1dc000000000000000000000000@0000000000aa36a7","amount_in":"0x64","asset_out":"58372ab62269a52fa636ad7f200d93999595dcaf000000000000000000000000@0000000000aa36a7","amount_out":"0x64","chain_id":[0,0,0,0,0,170,54,167]}'
+        expected_user = f"{to_vec(user.public_key)}".replace(" ", "")
+        expected = f'{{"tplus_user":{expected_user},"calldata":[],"asset_in":"62622e77d1349face943c6e7d5c01c61465fe1dc000000000000000000000000@0000000000aa36a7","amount_in":"0x64","asset_out":"58372ab62269a52fa636ad7f200d93999595dcaf000000000000000000000000@0000000000aa36a7","amount_out":"0x64","chain_id":[0,0,0,0,0,170,54,167]}}'
         assert actual == expected
 
         # Show it is the same as the inner version.
         actual = settlement.inner.signing_payload()
         assert actual == expected
 
+    def test_from_signed(self, settlement, user):
+        settlement = TxSettlementRequest.create_signed(settlement, user)
+        assert settlement.signature  # truthiness
+
 
 class TestBundleSettlementRequest:
-    def test_signing_payload(self):
+    def test_signing_payload(self, user):
         """
         Show we serialize to the JSON the clearing-engine expects.
         """
         inner = {
             "settlements": [get_base_settlement_data()],
             "bundle": {"bundle": {}, "bundle_id": 0},
-            "tplus_user": SETTLER,
+            "tplus_user": user.public_key,
             "chain_id": CHAIN_ID,
         }
         settlement = BundleSettlementRequest(inner=inner, signature=[])
         actual = settlement.signing_payload()
-        expected = '{"settlements":[{"asset_in":"62622e77d1349face943c6e7d5c01c61465fe1dc000000000000000000000000@0000000000aa36a7","amount_in":"0x64","asset_out":"58372ab62269a52fa636ad7f200d93999595dcaf000000000000000000000000@0000000000aa36a7","amount_out":"0x64"}],"bundle":{"bundle":{},"bundle_id":0},"chain_id":[0,0,0,0,0,170,54,167],"tplus_user":[235,136,106,86,249,240,239,166,68,50,103,140,235,241,39,14,147,20,167,88,230,235,105,122,96,98,2,164,81,227,232,46]}'
+        expected_user = f"{to_vec(user.public_key)}".replace(" ", "")
+        expected = f'{{"settlements":[{{"asset_in":"62622e77d1349face943c6e7d5c01c61465fe1dc000000000000000000000000@0000000000aa36a7","amount_in":"0x64","asset_out":"58372ab62269a52fa636ad7f200d93999595dcaf000000000000000000000000@0000000000aa36a7","amount_out":"0x64"}}],"bundle":{{"bundle":{{}},"bundle_id":0}},"chain_id":[0,0,0,0,0,170,54,167],"tplus_user":{expected_user}}}'
         assert actual == expected
 
 
