01d74204f4a7f755baed693082eb47660a6b23b2
jmcph4 <jack@jmcph4.dev>
2025-10-24 17:55:02 +1000

    chore: imports

--- tests/model/test_trades.py
+++ tests/model/test_trades.py
@@ -1,8 +1,7 @@
 import json
-from pydantic import TypeAdapter
 
 from tplus.model.asset_identifier import AssetIdentifier
-from tplus.model.trades import TradeEvent, parse_trade_event, TradeConfirmedEvent
+from tplus.model.trades import parse_trade_event, TradeConfirmedEvent
 
 def test_parse_trade_event():
     raw_msg = '{"Confirmed":{"asset_id":"82af49447d8a07e3bd95bd0d56f35241523fbab1000000000000000000000000@000000000000a4b1","trade_id":122519,"price":"3862.36934070","quantity":"0.01000000","timestamp_ns":1761283013888453220,"buyer_is_maker":false,"status":"Confirmed"}}'
