5dfec8c680f0600c75470fb61792237dea69a9f9
Aishwarya MANORE <manore.aishwarya@gmail.com>
2026-02-04 16:59:47 +0100

    tpluspy: Ruff format

--- tplus/model/user_solvency.py
+++ tplus/model/user_solvency.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 
 class AccountSolvency(BaseModel):
     """Represents solvency of  a single user account"""
+
     """Example: {'is_solvent': True, 'distance_from_liquidation': '400000.000000'}"""
     is_solvent: bool
     distance_from_liquidation: int
@@ -10,6 +11,7 @@ class AccountSolvency(BaseModel):
 
 class UserSolvency(BaseModel):
     """Represents a single user solvency: collection of solvency info of all user accounts"""
+
     """ Example: {'accounts': {'1': {'is_solvent': True, 'distance_from_liquidation': '400000.000000'}}}"""
     accounts: dict[int, AccountSolvency]
 
@@ -20,7 +22,7 @@ def parse_user_solvency(data: dict) -> UserSolvency:
     for account_id, account_data in data.get("accounts", {}).items():
         accounts[int(account_id)] = AccountSolvency(
             is_solvent=account_data["is_solvent"],
-            distance_from_liquidation=int(float(account_data["distance_from_liquidation"]))
+            distance_from_liquidation=int(float(account_data["distance_from_liquidation"])),
         )
 
     return UserSolvency(accounts=accounts)
