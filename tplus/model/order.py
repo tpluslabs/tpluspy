bdb8b72db749431abca80acd42a546f3bcd3082c
Serge Farny <serge.farny@gmail.com>
2026-03-11 11:38:18 +0100

    order: add is_reduce_only to response

--- tplus/model/order.py
+++ tplus/model/order.py
@@ -115,6 +115,7 @@ class OrderResponse(BaseModel):
     is_immediate_or_cancel: bool | None = None
     is_fill_or_kill: bool | None = None
     is_liquidation: bool | None = None
+    is_reduce_only: bool | None = None
 
 
 def parse_orders(orders_data: list[dict[str, Any]]) -> list[OrderResponse]:
