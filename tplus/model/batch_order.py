b9ccc588fea89d94d7bfb66dce961312bd4116c5
Aishwarya MANORE <manore.aishwarya@gmail.com>
2026-04-09 16:10:48 +0200

    tpluspy: Add batch order model.

---
+++ tplus/model/batch_order.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+
+from tplus.model.order import CreateOrderRequest
+
+
+class BatchCreateOrderRequest(BaseModel):
+    orders: list[CreateOrderRequest]
+
+class SingleOrderFromBatch(BaseModel):
+    order_id: str
+    status: str
+    reason: str|None
+
+class BatchCreateOrderRequestResponse(BaseModel):
+    batch_create_order_request_response: list[SingleOrderFromBatch]
+
+def parse_batch_order_response(parsed_batch_order_response: dict) -> BatchCreateOrderRequestResponse:
+    list_of_single_orders_from_batch: list[SingleOrderFromBatch] = []
+    results = parsed_batch_order_response['results']
+    for result in results:
+        single_order_from_batch = SingleOrderFromBatch(
+            order_id=result["order_id"],
+            status=result["status"],
+            reason=result["reason"]
+        )
+        list_of_single_orders_from_batch.append(single_order_from_batch)
+
+    return BatchCreateOrderRequestResponse(batch_create_order_request_response=list_of_single_orders_from_batch)
