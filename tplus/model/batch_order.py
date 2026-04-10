08bca4b1d9e7e4db463d8e6c1f3637d82b6b694c
Aishwarya MANORE <manore.aishwarya@gmail.com>
2026-04-10 09:48:51 +0200

    tpluspy: Address comments.

--- tplus/model/batch_order.py
+++ tplus/model/batch_order.py
@@ -7,23 +7,23 @@ class BatchCreateOrderRequest(BaseModel):
     orders: list[CreateOrderRequest]
 
 
-class SingleOrderFromBatch(BaseModel):
+class SingleOrderStatusFromBatch(BaseModel):
     order_id: str
     status: str
     reason: str | None
 
 
 class BatchCreateOrderRequestResponse(BaseModel):
-    batch_order_status: list[SingleOrderFromBatch]
+    batch_order_status: list[SingleOrderStatusFromBatch]
 
 
 def parse_batch_order_response(
     parsed_batch_order_response: dict,
 ) -> BatchCreateOrderRequestResponse:
-    list_of_single_orders_from_batch: list[SingleOrderFromBatch] = []
+    list_of_single_orders_from_batch: list[SingleOrderStatusFromBatch] = []
     results = parsed_batch_order_response["results"]
     for result in results:
-        single_order_from_batch = SingleOrderFromBatch(
+        single_order_from_batch = SingleOrderStatusFromBatch(
             order_id=result["order_id"], status=result["status"], reason=result["reason"]
         )
         list_of_single_orders_from_batch.append(single_order_from_batch)
